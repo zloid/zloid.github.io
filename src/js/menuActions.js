d80fc745f6d0c124cb5148f38bdd87d87d54ab4e
zloid <zloid-1@yandex.by>
2019-08-29 18:57:42 +0200

    JQuery thrown away

--- src/js/menuActions.js
+++ src/js/menuActions.js
@@ -1,39 +1,51 @@
 'use strict'
 
+function animationForDiv(nodeList, speed = 0.5) {
+  nodeList.forEach(e => {
+    e.style.transform = 'scale(0,0)'
+    setTimeout(() => {
+      e.style.transition = `${speed}s ease-in-out`
+      e.style.transform = 'scale(1,1)'
+    }, 1)
+  })
+}
+
 function menuActionsViaImgs() {
   let hidden = document.querySelector('.hidden')
   hidden.style.display = 'none'
-
   let allButtons = document.querySelectorAll('#filter > button')
-  //todo
-  ////////////TODO/////////////
+  let allPosts = document.querySelectorAll('.post')
+  let postsDiv = document.querySelector('#posts')
+
   allButtons.forEach(e => {
     e.addEventListener('click', () => {
       let filter = e.className
 
-      if (filter === 'lall') {
-        ////////////////////////++++
-        $('.hidden')
-          .contents()
-          .appendTo('#posts')
-          .hide()
-          .show('slow')
-        ////////////////////////
+      if (filter !== 'lall') {
+        allPosts.forEach(e => {
+          hidden.appendChild(e)
+        })
+
+        let hiddenIn = document.querySelectorAll('.hidden > div')
+
+        hiddenIn.forEach(e => {
+          postsDiv.appendChild(e)
+        })
+
+        animationForDiv(allPosts, 0.5)
+
+        allPosts.forEach(e => {
+          !e.classList.contains(filter) && hidden.appendChild(e)
+        })
       } else {
-        ////////////////////////++++
+        let hiddenIn = document.querySelectorAll('.hidden > div')
 
-        $('.post').appendTo('.hidden')
+        hiddenIn.forEach(e => {
+          // console.log(e.getElementsByTagName('img')[0].src)
+          postsDiv.appendChild(e)
+        })
 
-        $('.hidden')
-          .contents()
-          .appendTo('#posts')
-          .hide()
-          .show('slow')
-
-        $('.post:not(.' + filter + ')')
-          .appendTo('.hidden')
-          .hide('slow')
-        ////////////////////////
+        animationForDiv(allPosts, 0.65)
       }
     })
   })
